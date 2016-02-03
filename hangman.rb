bfe06f67b0d982c49673539af791d2ec12260006
Diarmuid Murphy <diarmuid.com@gmail.com>
2016-02-03 08:08:12 +0000

    all working bar save/load

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,16 @@
+def find_replace_letter(word, letter, board)
+  position = 0
+  
+  while position < word.length
+    if letter == word[position]
+      board[position] = letter
+    end
+    position += 1
+  end
+  
+  return word, letter, board
+end
+
 def start_game
   continue = true
   
@@ -5,14 +18,46 @@ def start_game
   answer = gets.chomp
   word_array = []
   
-  if answer == "y"
+  if answer == "n"
     File.open("5desk.txt").each do |line|
-      if (line.length >= 7) and (line.length <= 14) # 5 letter words are len 7 (\r\n)
+      if (line.length >= 7) && (line.length <= 14) # 5 letter words are len 7 (\r\n)
         word_array.push(line.chomp.downcase) # remove new line chars
       end
     end
-  else
+    
+    word = word_array[rand(word_array.length)]
+    board = "_" * word.length
     lives_left = 10
+    
+    # puts word #this is the answer
+    puts board
+    puts "You have #{lives_left} lives left!"
+  else
+    # board and word and turns left are set equal to values loaded from csv
+  end
+  
+  while continue
+    puts "What letter would you like to guess?:"
+    letter = gets.chomp
+    
+    if word.include? letter
+      find_replace_letter(word, letter, board)
+    else
+      lives_left -= 1
+    end
+    
+    puts board
+    puts "You have #{lives_left} lives left!"
+    
+    if lives_left == 0
+      puts "YOU LOSE, GAME OVER!"
+      continue = false # just to end the program
+    end 
+    
+    unless board.include?("_")
+      puts "CONGRADULATIONS, YOU WIN!"
+      continue = false # just to end the program
+    end
   end
 end
 
